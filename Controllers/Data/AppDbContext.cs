207d7741970df3a03116493184b018969e0e4202
bodie wood <bodie1@MacBook-Air.local>
2023-08-19 10:54:37 -0700

    Add db configuration and their properties.

--- Controllers/Data/AppDbContext.cs
+++ Controllers/Data/AppDbContext.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SchoolManagerProject.Models;
 
 namespace SchoolManagerProject.Data
 {
 
- public class AppDbContext:DbContext
+ public class AppDbContext : DbContext
  {
-  public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+  //Constructor: connects to the database
+  public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
   {
-   
   }
+
+ //Configure the relationships between the tables. 
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+
+   //Composite key for Actor_Movie.
+   modelBuilder.Entity<Actor_Movie>()
+   .HasKey(am => new
+   {
+    am.ActorId,
+    am.MovieId
+   });
+
+   //One movie can have many actors.
+   modelBuilder.Entity<Actor_Movie>()
+    .HasOne(m => m.Movie)
+    .WithMany(am => am.Actors_Movies)
+    .HasForeignKey(m => m.MovieId);
+
+   //One actor can have many movies.
+   modelBuilder.Entity<Actor_Movie>()
+    .HasOne(m => m.Actor)
+    .WithMany(am => am.Actors_Movies)
+    .HasForeignKey(m => m.ActorId);
+
+   //Makes sure default behavior is applied, along with any custom configurations.
+   base.OnModelCreating(modelBuilder);
+  }
+
+  //Create the tables in the database.
+  //Allows me to access and interact with the tables in the database.
+  public DbSet<Actor> Actors { get; set; }
+  public DbSet<Movie> Movies { get; set; }
+  public DbSet<Actor_Movie> Actor_Movies { get; set; }
+  public DbSet<Cinema> Cinemas { get; set; }
+  public DbSet<Producer> Producers { get; set; }
  }
 }
